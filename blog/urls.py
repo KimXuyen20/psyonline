ca2169d7131a8a24c940f3cd84a8e3a31d3aa89a
KimXuyen20 <nguyenthikimxuyen202002@gmail.com>
2024-11-20 16:15:46 +0700

    xuyen

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.blog, name='blog'),
+    path('<slug:blog_slug>/', views.blog_detail,name="blog_detail")
    
 ]
--- blog/views.py
+++ blog/views.py
@@ -9,3 +9,15 @@ def blog(request):
         'blogs':blogs,
     }
     return render(request, 'blog/blog.html',context)
+
+
+def blog_detail(request, blog_slug):
+    try:
+        single_blog = Blog.objects.get(slug = blog_slug)
+    except Exception as e:
+        raise e
+    
+    context = {
+        'single_blog':single_blog,
+    }
+    return render(request,'blog/blog_detail.html',context)
