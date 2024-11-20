d333687ed527d8016634c065dde2d60f2e8cc29f
KimXuyen20 <nguyenthikimxuyen202002@gmail.com>
2024-11-20 19:42:58 +0700

    xuyen

--- blog/models.py
+++ blog/models.py
@@ -5,8 +5,9 @@ from django.db import models
 class Blog(models.Model):
     blog_name = models.CharField(max_length=500, unique=True)
     slug = models.SlugField(max_length=500, unique=True)
-    description = models.TextField(max_length=1000, blank=True)
+    description = models.TextField(blank=True)
     images = models.ImageField(upload_to='photos/blogs')
+    blog_body = models.TextField(max_length=5000)
     is_available = models.BooleanField(default=True)
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now= True)
